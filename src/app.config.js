ea192d5e0d936debf186fa1994e8028aefd9890c
Hemanth-Jampala <jampala.hemanth26@gmail.com>
2022-09-01 06:57:45 +0000

    chnaged team name

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'CloudTeam3';
+    team = 'CloudTeamThree';
     version = '1.0.0';
 }
 module.exports = AppConfig;
